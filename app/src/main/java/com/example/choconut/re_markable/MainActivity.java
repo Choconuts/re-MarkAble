e8c42852c8d11aa436951341e5a6bd513aa10b00
choconuts <843940285@qq.com>
2018-10-31 16:42:28 +0800

    establish

---
+++ app/src/main/java/com/example/choconut/re_markable/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.choconut.re_markable;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
